dd2436af4e9febbdaf0d230a06cded881c2b4b0b
Su-Yeol Jeon <devxoul@gmail.com>
2011-09-07 14:46:04 +0900

    Initial Commit

---
+++ Sonaki/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Sonaki
+//
+//  Created by 전 수열 on 11. 9. 7..
+//  Copyright Joyfl 2011년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
